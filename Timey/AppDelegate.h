6a4e65096b2000cfe99c1201514b0b5221a73151
Jader Feijo <jader@movinpixel.com>
2013-11-18 11:05:10 +0000

    Initial Commit

---
+++ Timey/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Timey
+//
+//  Created by Jader Feijo on 18/11/2013.
+//  Copyright (c) 2013 Jader Feijo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Timey/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Timey
+//
+//  Created by Jader Feijo on 18/11/2013.
+//  Copyright (c) 2013 Jader Feijo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
